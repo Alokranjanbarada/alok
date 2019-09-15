1299de78991f3acc05f0638f2d2347bd590d7726
Alok <Alok@DESKTOP-C8KBFVL>
2019-09-15 02:18:59 -0700

    second commit...

---
+++ src/alok12/Free.java
@@ -0,0 +1,13 @@
+package alok12;
+
+public class Free {
+
+	static int l=12,w=10;
+	static int A=l*w;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("wlcm to java world");
+System.out.println(A);
+	}
+
+}
